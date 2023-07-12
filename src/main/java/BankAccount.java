395446d74b5db79bd587cef0f8b0ab2e249f93c7
Maymunah Waqas Butt <maymunahwaqas@outlook.com>
2023-07-12 15:52:42 +0100

    started mvp task

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class BankAccount {
+    public String firstName;
+    public String lastName;
+    public int dateOfBirth;
+    public LocalDate Date;
+    public int accountNumber;
+    public int balance;
+
+
+}
