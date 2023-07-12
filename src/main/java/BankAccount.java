51028f08dd0616002a8b8416ad26be887cdd250a
Maymunah Waqas Butt <maymunahwaqas@outlook.com>
2023-07-12 19:34:42 +0100

    getted and setted other variables, also added deposit method, also tested everything

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,12 +1,59 @@
-import java.time.LocalDate;
-
 public class BankAccount {
-    public String firstName;
-    public String lastName;
-    public int dateOfBirth;
-    public LocalDate Date;
-    public int accountNumber;
-    public int balance;
+
+//    public BankAccount(String firstName, String lastName, ){
+//        this.name = inputName;
+//        this.town = inputTown;
+//        this.completedCourse = false;
+//    }
+
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String currentDate;
+    private int accountNumber;
+    private int balance;
 
 
+    public BankAccount(String firstName, String lastName, String dateOfBirth, String currentDate, int accountNumber, int balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.currentDate = currentDate;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void addBalance(int deposit) {
+        this.balance += deposit;
+    }
+
+    public int deposit() {
+        return balance + 1000;
+    }
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,3 +1,61 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
-    
+
+    BankAccount bankaccount;
+    @BeforeEach
+    public void setUp() {
+        bankaccount = new BankAccount("Maymunah", "Butt", "22/08/02", "12/07/23", 171717, 0 );
+    }
+    @Test
+    public void hasFirstName(){
+        String actual = bankaccount.getFirstName();
+        String expected = "Maymunah";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void hasLastName(){
+        String actual = bankaccount.getLastName();
+        String expected = "Butt";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void hasDateOfBirth(){
+        String actual = bankaccount.getDateOfBirth();
+        String expected = "22/08/02";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void hasCurrentDate(){
+        String actual = bankaccount.getCurrentDate();
+        String expected = "12/07/23";
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void setAccountNumber(){
+        int actual = bankaccount.getAccountNumber();
+        int expected = 171717;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void setBalance(){
+        int actual = bankaccount.getBalance();
+        int expected = 0;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void deposit(){
+        int actual = bankaccount.deposit();
+        int expected = 1000;
+        assertThat(actual).isEqualTo(expected);
+    }
 }
